5d1a204ef5fd493c7dc84eaa71f13c75b96cd662
Nguyen Nam <60035507+tavenguyen@users.noreply.github.com>
2025-11-23 10:40:36 +0700

    (add) model predicts spam/ham mail.

--- script.py
+++ script.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
 import nltk
+import math
 from nltk.tokenize import word_tokenize
 
 # nltk.download('punkt_tab')
 
 global_vocab = dict()
 spam_dict = {}
+ham_dict = {}
 total_samples = 0
 total_spam_mails = 0
 total_words = 0
 total_words_in_spam = 0
+total_words_in_ham = 0
 
 ##################################################################
 file_obj = open("SMSSpamCollection.txt", mode = 'r', encoding = 'utf-8')
@@ -33,9 +36,12 @@ while (1):
         total_spam_mails += 1
         for w in words:
             total_words_in_spam += 1
-            if w not in spam_dict:
-                spam_dict[w] = 0
-            spam_dict[w] += 1
+            spam_dict[w] = spam_dict.get(w, 0) + 1
+    else:
+        for w in words:
+            total_words_in_ham += 1
+            ham_dict[w] = ham_dict.get(w, 0) + 1
+    
     total_samples += 1
 
 file_obj.close()
@@ -43,18 +49,37 @@ file_obj.close()
 ##################################################################
 vocab_size = len(global_vocab)
 
-# P(Word | Spam) = (Count(W_i in Spam) + 1) / (Total Words In Spam + V)
-# P(Spam | Word) = P(Word | Spam) . P(Spam) / P(Word)
-def calculateProbabilityOfWord(word):
-    probability_spam = total_spam_mails / total_samples
-    probability_word = global_vocab.get(word, 0) / total_words
-    probability_word_spam = (spam_dict.get(word, 0) + 1) / (total_words_in_spam + vocab_size)
-    return probability_word_spam * probability_spam / probability_word
+# P(Word | Ham), P(Word | Spam)
+def calculateProbability(word, type : bool):
+    # Spam
+    if type == False:
+        return (spam_dict.get(word, 0) + 1) / (total_words_in_spam + vocab_size)
+    else:
+        return (ham_dict.get(word, 0) + 1) / (total_words_in_ham + vocab_size)
 
-sentence = "Free money for your live!"
+sentence = "You have 1 new message. Call 12345678"
 words = nltk.tokenize.word_tokenize(sentence)
-probability = 1.0
-for word in words:
-    probability *= calculateProbabilityOfWord(word)
 
-print("Probability of", sentence, "to be 'spam email' is:", probability) 
+# P(Spam)
+probability_spam = total_spam_mails / total_samples
+
+# P(Ham)
+probability_ham = 1.0 - probability_spam
+
+# Score_{spam}
+score_spam = probability_spam
+for word in words:
+    score_spam  += math.log(calculateProbability(word, False))
+
+# Score_{ham}
+score_ham = probability_ham
+for word in words:
+    score_ham += math.log(calculateProbability(word, True))
+
+print('Score_{ham}:', score_ham, 'Score_{spam}:',score_spam)
+if score_ham > score_spam:
+    print(sentence, ": ham")
+else:
+    print(sentence, ": spam")
+
+
